d3bbad2bb1c9ae1228cd9d75b8b006334d3b2970
Łukasz <lukaszkus77@gmail.com>
2024-10-05 12:42:23 +0200

    update

--- components/Nav/Logo.jsx
+++ components/Nav/Logo.jsx
@@ -1,6 +1,6 @@
 import classes from "./Logo.module.scss";
 import Image from "next/image";
-import logo from "../../assets/image/g-wat_transparent.png";
+import logo from "../../assets/image/g-wat4-transparent.png";
 
 const Logo = ({ showLogo }) => {
   // const logoStyle = {
