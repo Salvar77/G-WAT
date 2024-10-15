423614ca43af7c2be6eb02fb08bcde1b7078332f
Łukasz <lukaszkus77@gmail.com>
2024-10-15 18:46:13 +0200

    update

--- components/Nav/Logo.jsx
+++ components/Nav/Logo.jsx
@@ -1,6 +1,6 @@
 import classes from "./Logo.module.scss";
 import Image from "next/image";
-import logo from "../../assets/image/g-wat11.png";
+import logo from "../../assets/image/g-wat13.png";
 import Link from "next/link";
 
 const Logo = () => {
