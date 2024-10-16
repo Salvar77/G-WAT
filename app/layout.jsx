36b8ee4b0887ee5f8091a84f75752293610c8260
Łukasz <lukaszkus77@gmail.com>
2024-10-16 23:16:02 +0200

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,15 +19,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {typeof window !== "undefined" && (
-          <script
-            id="Cookiebot"
-            src="https://consent.cookiebot.com/uc.js"
-            data-cbid="4ff350f7-5c40-43d1-8e72-520c9043fac2"
-            data-blockingmode="auto"
-            type="text/javascript"
-          ></script>
-        )}
+        <script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="4ff350f7-5c40-43d1-8e72-520c9043fac2"
+          data-blockingmode="auto"
+          type="text/javascript"
+        ></script>
+
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
