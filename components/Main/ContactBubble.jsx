2f3c3d74ab0370dfd53bfa78d680970b6b8ea31b
Łukasz <lukaszkus77@gmail.com>
2024-11-12 23:28:29 +0100

    update

--- components/Main/ContactBubble.jsx
+++ components/Main/ContactBubble.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import classes from "./ContactBubble.module.scss";
-
 const ContactBubble = () => {
   const [isExpanded, setIsExpanded] = useState(false);
 
