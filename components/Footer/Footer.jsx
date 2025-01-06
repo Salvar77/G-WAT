81962aa346d94aace049e26e5b6095e80986ff8f
Łukasz <lukaszkus77@gmail.com>
2025-01-06 16:00:58 +0100

    update

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import GoogleMap from "../More/GoogleMap";
+import { contactInfo } from "../../constants/index";
 import classes from "./Footer.module.scss";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
+  const { address, phone, email } = contactInfo;
 
   return (
     <div>
@@ -11,9 +13,23 @@ const Footer = () => {
         <div className={classes.footer__container}>
           <div className={classes.footer__text}>
             <p className={classes.footer__textInfo}>
-              &copy;{currentYear} G-WAT
+              {address.street}, {address.city} | Tel: {phone.number} |{" "}
+              <a href={`mailto:${email.address}`}>{email.address}</a>
             </p>
           </div>
+
+          <ul className={classes.footer__menu}>
+            <li>
+              <a href="/technologie">Technologie</a>
+            </li>
+            <li>
+              <a href="/galeria">Realizacje</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={classes.footer__text}>
+          <p className={classes.footer__textInfo}>&copy;{currentYear} G-WAT</p>
         </div>
       </footer>
     </div>
